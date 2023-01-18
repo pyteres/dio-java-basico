49fac5d4d57d3c9e5584924ac2c6c6bec2f06ae1
pyteres <ianpimenta18@gmail.com>
2023-01-18 11:56:42 -0300

    sobre-identaçao

--- Meu-Primeiro-Projeto-Java/java-anatomia-classes/src/MinhaClasse.java
+++ Meu-Primeiro-Projeto-Java/java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,38 @@
 public class MinhaClasse {
 public static void main(String[] args){
-   string Br = "brasil"
+// esee e um metodo executavel
+   //System.out.print("ola turma, sejam bem vindos");
+
+   // Padrao de nomenclatura
+   //todo class .java deve ter todas as primeiras letras das palavras maiusculas.
+   // o nome da classe deve ser igual a do arquivo java.
+   //toda variavel composta deve ter a primeira letra da segunda palvra maiuscula. Essa pratica se chama CamelCase.
+   // Existe uma palavra nas variaveis que mostra que ela nao pode mudar de valor.
+   //Essa palvra e a plavra "final". mais se voce nao quiser essa palvra diretamente e  so colocar a variavel com a primeira letra maiuscula.
+   //exemplos
+   //int ano = 2021;
+   //int ano = 2022;
+    //final String BR = "Brasil";
+   //BR = "Brazil";
+   
+   //Estrutura para diferenciar uma variavel de um metodo.
+   //Estrutura para declarar uma variavel:
+   //A variavel tem: tipo, nome, valor.
+   //exemplo
+   //String meuNome = "Ian";
+   //int anoDeFabricaçao = 2002;
+   //boolean verdadeira = false;
+   //anoDeFabricaçao = 2004;
+
+   //estrutura metodo:
+   //exemplo
+   //String primeiroNome = "Ian";
+   //String segundoNome = "Pimenta";
+   //String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+   //System.out.print(nomeCompleto);
 }
+//public static String nomeCompleto (String primeiroNome, String segundoNome){
+   //return "resultado do metodo" + primeiroNome.concat(segundoNome);
+//}   
 }
+
