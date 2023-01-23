b8e72b48a767f84c02d5bd18f5e5bd3e0cf8e7cb
pyteres <ianpimenta18@gmail.com>
2023-01-23 03:56:51 -0300

    java-primeria-parte

---
+++ Meu-Primeiro-Projeto-Java/java-anatomia-classes/src/edu/ian/primeriasemana/BoletimEstudantil.java
@@ -0,0 +1,14 @@
+package edu.ian.primeriasemana;
+public class BoletimEstudantil {
+ //identaçao e a forma de deixar o seu codigo mais organizado.   
+   public static void main(String[] args){
+    int mediaFinal = 6;
+    if(mediaFinal < 6){
+    System.out.print("Reprovado");
+    }else if(mediaFinal == 6){
+        System.out.print("Prova Minerva");
+    }else{
+        System.out.print("Aprovado");
+    }
+   } 
+}
---
+++ Meu-Primeiro-Projeto-Java/java-anatomia-classes/src/edu/ian/segundasemana/MinhaClasse.java
@@ -0,0 +1,53 @@
+package edu.ian.segundasemana;
+public class MinhaClasse {
+public static void main(String[] args){
+// esee e um metodo executavel
+   //System.out.print("ola turma, sejam bem vindos");
+
+   // Padrao de nomenclatura
+   //todo class .java deve ter todas as primeiras letras das palavras maiusculas.
+   // o nome da classe deve ser igual a do arquivo java.
+   //toda variavel composta deve ter a primeira letra da segunda palvra maiuscula. Essa pratica se chama CamelCase.
+   // Existe uma palavra nas variaveis que mostra que ela nao pode mudar de valor.
+   //Essa palvra e a plavra "final". mais se voce nao quiser essa palvra diretamente e  so colocar a variavel com a primeira letra maiuscula.
+   //exemplos
+   //int ano = 2021;
+   //int ano = 2022;
+    //final String BR = "Brasil";
+   //BR = "Brazil";
+   
+   //Estrutura para diferenciar uma variavel de um metodo.
+   //Estrutura para declarar uma variavel:
+   //A variavel tem: tipo, nome, valor.
+   //exemplo
+   //String meuNome = "Ian";
+   //int anoDeFabricaçao = 2002;
+   //boolean verdadeira = false;
+   //anoDeFabricaçao = 2004;
+
+   //estrutura metodo:
+   //exemplo
+   //String primeiroNome = "Ian";
+   //String segundoNome = "Pimenta";
+   //String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+   //System.out.print(nomeCompleto);
+}
+//public static String nomeCompleto (String primeiroNome, String segundoNome){
+   //return "resultado do metodo" + primeiroNome.concat(segundoNome);
+//}
+//Pacotes: Sao pastas que deixa o seu projeto de forma mais orognazada.
+//Java Beans: e uma forma de tornar o seu codigo mais comprensivel.
+//Primeiro ponto sobre isso: 
+//variaveis:
+//Quando voce for escrever uma variavel escreva o nome da varaiavel que faça sentido. As variaveis sempre tem que ter nomes no singular elas so podem ser no plural se for arrays ou coleçoes. E se voce escreve uma variavel em um idioma voce deve escrever todas as variaveis neste mesmo idioma.
+//exemplos: jeito errado
+//double salMedio = 1500.40; //O valor da variavel nao pode ser abreviado.
+//String email = "aluno@escola.com"; //Confuso ja que a variavel nao esta vem definida.
+//String meuNome = "joseph"; //Se voce poe uma variavel em porugues ou ingles nao pode mudar por outra variavel com outro idioma.
+//exemplos: jeito certo
+//double salarioMedio = 1500.40;
+//String email = "aluno@escola.com";
+//String [] emails = {"aluno@escola.com", "professor@escola.com", "secretaria@escola.com"}; 
+//String meuNome = "joseph";
+}
+
