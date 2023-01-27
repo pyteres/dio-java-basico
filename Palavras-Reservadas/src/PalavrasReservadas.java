39df918c5e8b780771897c7aabc2b8b030520f90
pyteres <ianpimenta18@gmail.com>
2023-01-27 10:04:58 -0300

    sexta-parte

---
+++ Palavras-Reservadas/src/PalavrasReservadas.java
@@ -0,0 +1,10 @@
+public class PalavrasReservadas {
+//Palavrads reservadas: Sao indentificadores de uma linguagem que ja possui uma finalidade especifica, Portanto nao podem ser usadas para declarar classes variaveis metodos ou atributos.
+//A linguagem java possui 52 palavras reservadas. Todas essas palavras sao clasificadas em grupos e escritas com letras maiusculas, sendo indentificada com uma cor especial na maioria das IDES. Abaixo temos a lista de palavras organizadas em grupos e as suas finalidades.
+//Palabras reservadas: 
+//Arquivo: Packge import static esse e a forma de importar arquivos.
+//Classes: public protect private + fianl ou abstract + estends ou emplements.
+//Metodos; public protect private + static ou final ou abstract + void e return.
+//Atributo: public ou private ou protect + final ou static + tipo primitivo.
+
+}
